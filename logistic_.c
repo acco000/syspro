ca8eb53a2e8fdceb58b9957175759ee6ed4c61c7
acco <k338115@kansai-u.ac.jp>
2022-05-10 15:10:22 +0900

    Initial repo

---
+++ logistic_.c
@@ -0,0 +1,8 @@
+
+float logistic_(float *x) 
+{
+    float y;
+
+    y = 3.8 * (*x) * (1- (*x));
+    return y;
+}
---
+++ logistic_.h
@@ -0,0 +1,11 @@
+#if defined(__cplusplus)
+#define __BEGIN_DECLS extern "C" {
+#define __END_DECLS	}
+#else
+#define __BEGIN_DECLS
+#define __END_DECLS
+#endif
+
+__BEGIN_DECLS
+ float logistic_(float *);
+__END_DECLS
